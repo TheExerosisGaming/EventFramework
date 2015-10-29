a47391e60126392c71d316749f818553bbcafa33
Exerosis <theexerosisgaming@gmail.com>
2015-10-29 16:44:41 -0400

    ~Finished New Event System

---
+++ src/main/java/me/exerosis/event/EventListener.java
@@ -0,0 +1,48 @@
+package me.exerosis.event;
+
+import me.exerosis.component.event.Priority;
+
+public class EventListener<A, B> {
+    private Class<A> _listenerType;
+    private Priority _priority = Priority.NORMAL;
+    private boolean _post;
+
+    public Class<A> getListenerType() {
+        return _listenerType;
+    }
+
+    public boolean isPost() {
+        return _post;
+    }
+
+    public Priority getPriority() {
+        return _priority;
+    }
+
+    public void onEvent(B event) {
+        new EventListener<String, Integer>() {
+            public void onEvent(Integer event) {
+
+            }
+        }.post().register(String.class);
+    }
+
+    public EventListener<A, B> post() {
+        _post = !_post;
+        return this;
+    }
+
+    public EventListener<A, B> priority(Priority priority) {
+        _priority = priority;
+        return this;
+    }
+
+    public void register(Class<A> listenerType) {
+        _listenerType = listenerType;
+        EventManager.registerListener(this);
+    }
+
+    public void unregister() {
+        EventManager.unregisterListener(this);
+    }
+}
